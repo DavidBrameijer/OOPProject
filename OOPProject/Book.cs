7fa0b3b20569f65c23e0ca07031c4429ddc9f9e2
DavidBrameijer <davidbrameijer@gmail.com>
2024-08-22 14:17:40 -0400

    Created Book Class and CheckedBook child class

---
+++ OOPProject/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProject
+{
+    internal class Book
+    {
+        //propeties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool Status { get; set; }
+        
+
+        //constructor
+        public Book(string _title, string _author, bool _status)
+        {
+            Title = _title;
+            Author = _author;
+            Status = _status;
+            
+        }
+
+    }
+}
